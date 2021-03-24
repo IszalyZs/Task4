14786c3f5a1935e9ae3c35222e0e0850f02c83bd
IszalyZs <iszalyzs@gmail.com>
2021-03-24 17:51:50 +0100

    Add project

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,47 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+        Map<String, Integer> competitorsWithSumPoints = new HashMap<>();
+        try {
+            File file = new File(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] cutLine = line.split(",");
+                int sumPoints = 0;
+                for (int i = 1; i < cutLine.length - 1; i++) {
+                    sumPoints += Integer.parseInt(cutLine[i]);
+                }
+                competitorsWithSumPoints.put(cutLine[0], sumPoints);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sortCompetitorsNameByPoints(competitorsWithSumPoints);
+    }
+
+    private List<String> sortCompetitorsNameByPoints(Map<String, Integer> competitorsWithSumPoints) {
+        List<String> sortedCompetitorsName;
+        sortedCompetitorsName = competitorsWithSumPoints
+                .entrySet()
+                .stream()
+                .sorted((Map.Entry<String, Integer> entryOne, Map.Entry<String, Integer> entryTwo) -> {
+                    return entryTwo.getValue().equals(entryOne.getValue()) ?
+                            entryTwo.getKey().compareTo(entryOne.getKey())
+                            : entryTwo.getValue().compareTo(entryOne.getValue());
+                })
+                .map(e -> e.getKey())
+                .collect(Collectors.toList());
+        return sortedCompetitorsName;
     }
 
 }
